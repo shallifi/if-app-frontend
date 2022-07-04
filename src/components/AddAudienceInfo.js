e60edc325ae0728a8df2e2dba3f7a7c77a3b7e7d
Benjamin <shallifi@hotmail.com>
2022-07-04 15:56:37 -0400

    handlechange function

--- src/components/AddAudienceInfo.js
+++ src/components/AddAudienceInfo.js
@@ -2,6 +2,23 @@
 
 function AudienceMember(){
 
+    // controlled form
+    const [formData, setFormData]  = useState({
+        name:"",
+        age:"",
+        household_income:"",
+        city:"",
+        first_time :""
+    }
+    )
+
+    // handlechange function
+    const handlechange = (e) => {
+        const {name,value} = e.target;
+        setFormData((formData) => ({..formData, [name]: value})
+        )
+    }
+
 return (
     <div>
         <p>testing the add audience info page</p>
